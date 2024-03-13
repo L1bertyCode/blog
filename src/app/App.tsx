08edc895d1a24fbbd784acad2a9141b1d1179a04
LC <libertycodeg@gmail.com>
2024-03-13 23:54:56 +0300

    cr a/st/: th,r,v

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,18 @@
-import { Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Suspense, useState } from "react";
+import { Route, Routes, Link } from "react-router-dom";
 
 import { NotFoundPage } from "@/pages/NotFoundPage";
 import { MainPage } from "@/pages/MianPage";
 import { ArticlesListPage } from "@/pages/ArticlesListPage";
 
 import "./styles/index.scss";
-import { Link } from "react-router-dom";
 
 export function App() {
+ const [theme, setTheme] = useState<
+  "app_light_theme" | "app_dark_theme"
+ >("app_light_theme");
  return (
-  <div className={"app"}>
+  <div className={`app ${theme}`}>
    <Link to="/">MainPage</Link>
    <Link to="/articles">Articles</Link>
 
@@ -24,6 +26,17 @@ export function App() {
      <Route path="*" element={<NotFoundPage />} />
     </Routes>
    </Suspense>
+   <button
+    onClick={() =>
+     setTheme(
+      theme === "app_light_theme"
+       ? "app_dark_theme"
+       : "app_light_theme"
+     )
+    }
+   >
+    change theme
+   </button>
   </div>
  );
 }
