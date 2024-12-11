3b863c2f029697e812aeea432e0f021ef4c6fec7
LC <libertycodeg@gmail.com>
2024-12-11 21:46:00 +0300

    cr st/:th/vrbl

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,18 @@
 import { Link, Route, Routes } from "react-router-dom";
 import s from "./App.module.scss";
 
+
 import { Suspense } from "react";
 import { ProfilePage } from "@/pages/ProfilePage";
 import { MainPage } from "@/pages/MainPage";
+import { NotFoundPage } from "@/pages/NotFoundPage";
+
+import "./style/index.scss";
 
 interface AppProps { };
 export const App = ({ }: AppProps) => {
   return (
-    <div className={s.app}>
+    <div className={`app`}>
       <nav>
         <Link to="/">Home</Link>
         <Link to="/profile">Profile</Link>
@@ -17,7 +21,7 @@ export const App = ({ }: AppProps) => {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/profile" element={<ProfilePage />} />
-          <Route path="*" element={<ProfilePage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Suspense>
     </div>
