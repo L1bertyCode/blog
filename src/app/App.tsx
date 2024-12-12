f8edd9399dfa08d4e53fb40f23f544a2a044bfc3
LC <libertycodeg@gmail.com>
2024-12-12 21:15:57 +0300

    cr sh/l/cN

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,17 +8,18 @@ import { MainPage } from "@/pages/MainPage";
 import { NotFoundPage } from "@/pages/NotFoundPage";
 import { useTheme } from "./context/useTheme/useTheme";
 import { Theme } from "./context/ThemeContect/ThemeContect";
+import { classNames } from "@/shared/lib/classNames/classNames";
 
 interface AppProps { };
 export const App = ({ }: AppProps) => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app app_${theme}_theme`}>
+    <div className={classNames(`app app_${theme}_theme`, {}, [])}>
       <nav>
         <Link to="/">Home</Link>
         <Link to="/profile">Profile</Link>
-        <button onClick={ toggleTheme}>Theme</button>
+        <button onClick={toggleTheme}>Theme</button>
       </nav>
       <Suspense fallback={<div>Loding...</div>}>
         <Routes>
