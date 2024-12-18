628bdeb4cd2bd749a1c41888a4fe3626beeae6a5
LC <libertycodeg@gmail.com>
2024-12-18 20:27:04 +0300

    ch p/NFP

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,10 +1,11 @@
+import { useTranslation } from "react-i18next";
 import s from "./NotFoundPage.module.scss";
 interface NotFoundPageProps { };
 const NotFoundPage = ({ }: NotFoundPageProps) => {
+  const { t } = useTranslation();
   return (
     <div className={s.notFoundPage}>
-      {/* eslint-disable-next-line */}
-      <div>NotFoundPage</div>
+      {t("Page Not Found")}
     </div>
   );
 };
