0d2d930130f33043e2b90b85b15b152099a5c70e
LC <libertycodeg@gmail.com>
2024-12-28 18:12:21 +0300

    cr e/Usr

--- src/app/providers/StoreProvder/config/store.ts
+++ src/app/providers/StoreProvder/config/store.ts
@@ -1,13 +1,18 @@
 import { configureStore, EnhancedStore, StoreEnhancer, ThunkDispatch, Tuple, UnknownAction } from '@reduxjs/toolkit';
 import { StateSchema } from './StateSchema';
 import { counterReducer } from '@/entities/Counter';
+import { userReducer } from '@/entities/User';
 export function createReduxStore(initialState?: StateSchema): EnhancedStore<any, UnknownAction, Tuple<[StoreEnhancer<{
   dispatch: ThunkDispatch<any, undefined, UnknownAction>;
 }>, StoreEnhancer]>> {
+
+  const rootReducers = {
+    counter: counterReducer,
+    user: userReducer
+
+  };
   return configureStore<StateSchema>({
-    reducer: {
-      counter: counterReducer
-    },
+    reducer: rootReducers,
     devTools: __IS_DEV__,
     preloadedState: initialState
   });
---
+++ src/entities/User/index.ts
@@ -0,0 +1,9 @@
+export {
+  userReducer,
+  userActions
+} from "./model/slices/userSlice";
+export type {
+  User,
+  UserSchema
+} from "./model/types/user";
+
